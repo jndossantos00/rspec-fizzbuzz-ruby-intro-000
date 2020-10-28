fedfa70691a93a8c403bbcfdae6e67460d06dac5
Jessica dos Santos <jndossantos00@gmail.com>
2020-10-28 18:27:55 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz (int)
-  if int % 3 == 0
-  expect  "Fizz"
+def fizzbuzz (number)
+  if number % 3 == 00 && number % 5 == 0
+    "Fizzbuzz"
+  elsif number % 3 == 0
+    "buzz"
+  else
+    number
+  end
 end
