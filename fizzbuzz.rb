0c01f50e67d17e33a4291d137c9c89c66fcaae92
Jessica dos Santos <jndossantos00@gmail.com>
2020-10-28 19:50:23 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz (number)
   if number % 3 == 0 && number % 5 == 0
-    "Fizz"
-  elsif number % 5 == 0
-    "Buzz"
-  elsif number % 3 && number % 5 == 0
     "Fizzbuzz"
+  elsif number % 3 == 0
+    "Fizz"
+  elsif number % 5 && number % 5 == 0
+    "Buzz"
 else
     nil
   end
